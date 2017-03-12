eac4797706ef0ca9f3d79d8bda31c090a2ae51ca
DM <djmedora@gmail.com>
2017-03-11 23:45:23 -0800

    added options to html

--- app.py
+++ app.py
@@ -11,18 +11,18 @@ def main():
 def showSignUp():
 	return render_template('signup.html')
 
-@app.route("/signUp", methods=["POST"])
-def signUp():
-	# read posted values from the UI
-	_name = request.form["inputName"]
-	_email = request.form["inputEmail"]
-	_password = request.form["inputPassword"]
+# @app.route("/signUp", methods=["POST"])
+# def signUp():
+# 	# read posted values from the UI
+# 	_name = request.form["inputName"]
+# 	_email = request.form["inputEmail"]
+# 	_password = request.form["inputPassword"]
 
-	# validate received values
-	if _name and _email and _password:
-		return json.dumps({"html":"<span>All fields good!</span>"})
-	else:
-		return json.dumps({"html":"<span>Fields not completed.</span>"})
+# 	# validate received values
+# 	if _name and _email and _password:
+# 		return json.dumps({"html":"<span>All fields good!</span>"})
+# 	else:
+# 		return json.dumps({"html":"<span>Fields not completed.</span>"})
 
 if __name__ == "__main__":
 	app.run()
