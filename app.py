61ee849546868f1cd197d01b2be0970026948a9c
DM <djmedora@gmail.com>
2017-03-22 12:08:49 -0700

    css styling for table

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, json
 import schedule_table
 import pandas as pd
+import random
 
 ################# NOTES #################
 # add alerts for exam schedule conflicts
@@ -32,12 +33,22 @@ def showSchedule():
     df = pd.DataFrame(index = ["8-11am", "11:30-2:30pm", "3-6pm", "7-10pm"], columns = ["Mon", "Tues", "Weds", "Thurs", "Fri"]).fillna(" ")
     for exam_tup in exams:
         df.loc[exam_tup[1], exam_tup[0]] = classname
+    df.columns = ["Mon 5/8", "Tues 5/9", "Weds 5/10", "Thurs 5/11", "Fri 5/12"]
+    df.style.apply(color)
     return render_template("schedule.html", name=showSchedule, data=df.to_html())
 
     # return render_template('schedule.htmal', exams=exams)
 
 row_data = schedule_table.main()
 
+
+def color(s):
+    boolcol = s != " "
+    colors = ['#BBE3FF', '#ff6347', '187,227,255', '220,170,225', '188,255,166', '255,231,166', '252,226,226', ]
+    randindex = random.random() * len(colors) // 1
+    randcolor = colors[randindex]
+    colors.remove(randcolor)
+    return ['background-color:' + randcolor if v else '' for v in boolcol]
 
 
 def search(day, time, classname):
