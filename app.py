951a06393e5f5db034103c4d927f5f4defd496df
DM <djmedora@gmail.com>
2017-04-14 15:43:38 -0700

    heokrue

--- app.py
+++ app.py
@@ -150,8 +150,10 @@ def search(day, time, classname):
 #       return json.dumps({"html":"<span>Fields not completed.</span>"})
 
 if __name__ == "__main__":
-    app.run()
     # app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
